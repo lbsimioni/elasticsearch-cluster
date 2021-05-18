92e90639c4a78a9f8ba14f10a445753d92254207
Leonardo Bucioli Simioni <lsimioni@ciandt.com>
2021-05-17 22:22:26 -0300

    spring project init

---
+++ src/main/java/br/com/elasticsearchcluster/elasticsearchcluster/ElasticsearchClusterApplication.java
@@ -0,0 +1,13 @@
+package br.com.elasticsearchcluster.elasticsearchcluster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchClusterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchClusterApplication.class, args);
+	}
+
+}
